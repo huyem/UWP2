61e34695c00efbda2ada9f7dd2d731d0ba25dde6
huyem <huyem1004@gmail.com>
2021-05-26 16:38:23 +0700

    Add project files.

---
+++ Models/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWP2.Models
+{
+    class Class1
+    {
+        private string email;
+        private string titell;
+        private string content;
+
+        public Class1(string email, string titell, string content)
+        {
+            Email = email;
+            Titell = titell;
+            Content = content;
+        }
+
+        public string Email { get => email; set => email = value; }
+        public string Titell { get => titell; set => titell = value; }
+        public string Content { get => content; set => content = value; }
+    }
+}
+
